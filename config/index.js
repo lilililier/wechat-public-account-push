f3fd20e9e233529d1c35514eba2de1de0bb13dcd
lilililier <113515976+lilililier@users.noreply.github.com>
2022-09-14 18:59:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -102,15 +102,15 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "4bPRR0RkOeL9f3vmfQipbxC-spk1xwMCxeMc7TdloSw",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-  CALLBACK_USERS: [
+  CALLBACK_USERS: [Dulce-historia
     {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "o_S-O58Zw_urzyYX3RnYLaz9KfDE",
     },
     // 你可以不断按格式往下增加
     // ...
@@ -123,9 +123,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "江苏",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "镇江",
 
   /** 重要节日相关 */
 
